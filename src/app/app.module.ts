4329b09aa9838f61caad9d7499119e22b7bb5193
Ryan Williams <codereese@gamil.com>
2024-02-07 20:49:35 -0500

    updated more

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 import { FoodComponent } from './components/food/food.component';
 import { HttpClientModule } from  '@angular/common/http';
 import { NutrientComponent } from './components/nutrient/nutrient.component';
